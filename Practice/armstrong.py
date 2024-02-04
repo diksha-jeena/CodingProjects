856fcf9a37f7e9ab3d3e6570a9ffa53bab83ea64
diksha-jeena <158588723+diksha-jeena@users.noreply.github.com>
2024-02-04 20:53:11 +0530

    Initial commit

---
+++ Practice/armstrong.py
@@ -0,0 +1,11 @@
+num = int(input("Enter any number to check if its an armstrong number or not :"))
+digit = int(input("Enter the number of digits :"))
+sum = 0
+while(num // 10 == 0):
+    temp = num % 10
+    num = num // 10
+    sum = sum + temp ** digit
+if(sum == num):
+    print("It is an armstrong number .")
+else:
+    print("It is not an armstrong number .")
