8a5d4cae10177c5231e18040fcbc7c53e1227795
diksha-jeena <158588723+diksha-jeena@users.noreply.github.com>
2024-02-13 20:20:16 +0530

    initial commit

---
+++ FlowOfControl/ques8_4.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+if(num1 % num2 == 0):
+    print(num1 , "is divisible by" , num2)
+else:
+    print(num1 , "is not divisible by" , num2)
