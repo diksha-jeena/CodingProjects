b1910c60b812931203c11ed50cdde85943fa67c6
diksha-jeena <158588723+diksha-jeena@users.noreply.github.com>
2024-02-16 20:15:05 +0530

    initial commit

---
+++ FlowOfControl/ques8_7.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+op = input("Choose any operator [+ - * / %] : ")
+if(op == '+'):
+    print("result =" , num1 + num2)
+elif(op == '-'):
+    print("result =" , num1 - num2)
+elif(op == '*'):
+    print("result =" , num1 * num2)
+elif(op == '/'):
+    print("result =" , num1 / num2)
+elif(op == '%'):
+    print("result =" , num1 % num2)
