1692c73eecdc9219e3a6f763a311f3e27c20a827
diksha-jeena <158588723+diksha-jeena@users.noreply.github.com>
2024-02-18 19:12:26 +0530

    initial commit

---
+++ FlowOfControl/ques8_9.py
@@ -0,0 +1,9 @@
+char = input("Enter a single character : ")
+if(char >= "A" and char <= "Z"):
+    print("This is a uppercase character ")
+elif(char >= "a" and char <= "z"):
+    print("This is a lowercase letter ")
+elif(char >= "0" and char <= "9"):
+    print("You entered a digit")
+else:
+    print("You entered a special character")
