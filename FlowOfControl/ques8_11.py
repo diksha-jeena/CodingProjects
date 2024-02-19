975918ad52ca21af25dc30d866d0f2073dc97952
diksha-jeena <158588723+diksha-jeena@users.noreply.github.com>
2024-02-19 21:58:11 +0530

    initial commit

--- FlowOfControl/ques8_11.py
+++ FlowOfControl/ques8_11.py
@@ -1,4 +1,5 @@
 num = int(input("Enter a number :"))
 for i in range(1 , 11):
-    # print("Table of ", num ," = " , num * i)
     print(num , "x" , i , "=" , num * i)
+
+
---
+++ FlowOfControl/ques8_12.py
@@ -0,0 +1,4 @@
+sum = 0 
+for i in range(1,8):
+    sum += i
+print("Sum of 7 natural numbers = ", sum)
