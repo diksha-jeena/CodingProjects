d961cc9f74f0806d5dbb93167982c3234e9338be
diksha-jeena <158588723+diksha-jeena@users.noreply.github.com>
2024-02-19 23:43:06 +0530

    method 2 added

--- Tuple/strConv.py
+++ Tuple/strConv.py
@@ -1,9 +1,14 @@
-name=("d","i","k","s","h","a")
-name2=""
+# Original list of characters
+name = ["d", "i", "k", "s", "h", "a"]
+
+# Method 1: Concatenate characters to form a string
+name2 = ""
 for i in name:
-    name2=name2+i
+    name2 = name2 + i
 print(name2)
-name3=[]
+
+# Method 2: Create a new list with individual characters
+name3 = []
 for i in name:
     name3.append(i)
 print(name3)
