fa19d604b640952d326b9aea4a74f082f4f645be
diksha-jeena <158588723+diksha-jeena@users.noreply.github.com>
2024-03-02 23:32:10 +0530

    checking for prime number

--- FlowOfControl/prime.py
+++ FlowOfControl/prime.py
@@ -1,8 +1,8 @@
 #checking for prime number
 num = int(input("Enter a number :"))
-lim = int(num / 2) + 1   #calculates limit for the loop
+lim = int(num / 2) + 1 
 for i in range(2 , lim):
-    rem = num % i   #remainder
+    rem = num % i
     if(rem == 0):
         print(num , "is not a prime number")
         break
