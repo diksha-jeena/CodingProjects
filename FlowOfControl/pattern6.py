84cf448069c87984d5d91db88ca130aed92bfd10
diksha-jeena <158588723+diksha-jeena@users.noreply.github.com>
2024-03-04 23:07:22 +0530

    Pascal's Triangle

---
+++ FlowOfControl/pattern6.py
@@ -0,0 +1,16 @@
+
+from math import factorial
+n = 4
+for i in range(n):
+    for j in range(n-i+1):
+ 
+        # for left spacing
+        print(end=" ")
+ 
+    for j in range(i+1):
+ 
+        # nCr = n!/((n-r)!*r!)
+        print(factorial(i)//(factorial(j)*factorial(i-j)), end=" ")
+ 
+    # for new line
+    print()
