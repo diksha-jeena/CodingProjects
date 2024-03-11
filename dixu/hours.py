21b54b4096694c3f9b0c319b7cabb34b634e3e60
diksha-jeena <158588723+diksha-jeena@users.noreply.github.com>
2024-03-11 22:21:36 +0530

    calculating hours per year/days

--- dixu/hours.py
+++ dixu/hours.py
@@ -1,9 +1,8 @@
-#days=int(input("enter the no. of days"))
-#hours=days*24
-#print(hours)
-
+days=int(input("enter the no. of days"))
+hours=days*24
+print("Hours per days" , hours)
 
 years=int(input("enter the no. of years"))
 days=years*365
 hours=days*24
-print(hours)
+print("Hours per years" , hours)
