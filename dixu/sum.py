c31160ab6a653242fd20b8a0be62c3c7cb2b6c06
diksha-jeena <158588723+diksha-jeena@users.noreply.github.com>
2024-03-17 20:29:23 +0530

    calculating sum of three

--- dixu/sum.py
+++ dixu/sum.py
@@ -2,4 +2,4 @@ num1=int(input("enter first number"))
 num2=int(input("enter second number"))
 num3=int(input("enter third number"))
 sum=num1+num2+num3
-print("sum=",sum)
+print("sum of three numbers =",sum)
