cd41feb4e6adb6564c9b322e537af4bc5418a231
diksha-jeena <158588723+diksha-jeena@users.noreply.github.com>
2024-03-25 23:10:03 +0530

    Using sets methods

--- dixu/setsMethods.py
+++ dixu/setsMethods.py
@@ -1,9 +1,9 @@
 A={10,20,30,40,50}
 B={70,60,40,100,80}
-#sets.add(10)
-#sets.clear()
-#sets.copy()
-#print(A.difference(B))
-#print(B.difference(A))
+sets.add(10)
+sets.clear()
+sets.copy()
+print(A.difference(B))
+print(B.difference(A))
 A.difference_update(B)
 print(A)
