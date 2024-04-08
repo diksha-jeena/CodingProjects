7dba5febee8bb627bb22d917cc3567c01a3fd4dd
diksha-jeena <158588723+diksha-jeena@users.noreply.github.com>
2024-04-08 23:41:23 +0530

    creating product function

---
+++ Functions/product.py
@@ -0,0 +1,9 @@
+def pro(a , b , c):
+    temp = a * b * c
+    return temp
+
+#main
+a = int(input("Enter the value of A :"))
+b = int(input("Enter the value of B :"))
+c = int(input("Enter the value of C :"))
+print("Product of given three numbers = " , pro(a,b,c))
