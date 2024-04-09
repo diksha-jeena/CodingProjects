1235ec4bc7a2d9e1704317d1e89742c8179ca3b4
diksha-jeena <158588723+diksha-jeena@users.noreply.github.com>
2024-04-09 22:38:54 +0530

    creating function to calculate difference

---
+++ Functions/difference.py
@@ -0,0 +1,8 @@
+def diff(a,b):
+    c = a - b
+    return c
+
+#main
+a = int(input("Enter the value of a :"))
+b = int(input("Enter the value of b :"))
+print("The difference between the given two numbers is : " , diff(a,b))
