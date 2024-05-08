8389f2c63cefa4c9dbedef13b6a94da8777675ac
diksha-jeena <158588723+diksha-jeena@users.noreply.github.com>
2024-05-08 20:22:42 +0530

    creating multiple functions
    calling each function inside the other

---
+++ Functions/multiplefun.py
@@ -0,0 +1,20 @@
+def str1():
+    a1 = "Dixu "
+    # print(a1)
+    str2(a1)
+    return a1
+
+def str2(a1):
+    a2 = a1 + " I "
+    # print(a2)
+    str3(a2)
+    return a2
+def str3(a2):
+    a3 = a2 + " love "
+    # print(a3)
+    str4(a3)
+    return a3
+def str4(a3):
+    a4 = a3 + " U "
+    print(a4)
+str1()
