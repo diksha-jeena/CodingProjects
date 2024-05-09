d5b46d355472d82fc4e83b70c2fd1622ce92b37b
diksha-jeena <158588723+diksha-jeena@users.noreply.github.com>
2024-05-09 20:15:06 +0530

    higher order function (string)

---
+++ Functions/hofStr.py
@@ -0,0 +1,11 @@
+def lowercase(txt):
+    return txt.lower()
+
+def uppercase(txt):
+    return txt.upper()
+
+def printtxt(a , b):
+    print(a)
+    print(b)
+
+printtxt(lowercase("Dixu") , uppercase("Dixu"))
