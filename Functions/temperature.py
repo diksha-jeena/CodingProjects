03253aa402c4f29ebc6be8884b362dd3a672a9ce
diksha-jeena <158588723+diksha-jeena@users.noreply.github.com>
2024-05-18 23:08:56 +0530

    converting celsius to fahrenheit and vice versa

---
+++ Functions/temperature.py
@@ -0,0 +1,9 @@
+def celsius_to_fahrenheit(c):
+    return (c * 9/5) + 32
+
+def fahrenheit_to_celsius(f):
+    return (f - 32) * 5/9
+
+# Main
+print(celsius_to_fahrenheit(39))
+print(fahrenheit_to_celsius(102))  
