14237c73ccff6819d5b836ac65aaf3993cf36b8c
diksha-jeena <158588723+diksha-jeena@users.noreply.github.com>
2024-05-22 23:56:47 +0530

    checking even and odd numbers

---
+++ Functions/evenodd.py
@@ -0,0 +1,9 @@
+def even_or_odd(number):
+    if number % 2 == 0:
+        return f"{number} is even."
+    else:
+        return f"{number} is odd."
+
+
+print(even_or_odd(5))  
+print(even_or_odd(10))  
