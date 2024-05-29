c398219aa8348f1a7020c9a8cc01772188f21ecd
diksha-jeena <158588723+diksha-jeena@users.noreply.github.com>
2024-05-29 22:19:58 +0530

    appending a new line in text file

--- File Handling/notes1.py
+++ File Handling/notes1.py
@@ -1,3 +1,6 @@
 with open('notes1.txt', 'w') as file:
     file.write("Balance Sheet - ")
     file.write("It is a statement of Assets, Liabilities and Equity of the company at a given date.\n")
+
+with open('notes1.txt' , 'a') as file :
+    file.write("Profit and loss (P&L)")
