a4971585c3d354f88a57895dcc044e0726957a82
diksha-jeena <158588723+diksha-jeena@users.noreply.github.com>
2024-06-11 23:35:38 +0530

    writing and reading a text file with flush function

--- File Handling/hello.py
+++ File Handling/hello.py
@@ -1,3 +1,8 @@
 with open('hello.txt' , 'w') as file:
     file.write("Welcome my class" '\n' "It is a fun place" '\n' "You will learn and play")
+    file.flush()
     
+with open('hello.txt' , 'r') as file:
+    content = file.read()
+    print(content)
+    file.flush()
